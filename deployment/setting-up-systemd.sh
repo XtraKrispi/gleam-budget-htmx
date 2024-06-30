90897fe28e8aed86c8b4baeda2fe8c4791aec1c7
Michael Gold <mgold@spscommerce.com>
2024-06-29 21:26:05 -0400

    Adding setup script

---
+++ deployment/setting-up-systemd.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+# https://www.howtogeek.com/687970/how-to-run-a-linux-program-at-startup-with-systemd/
+
+sudo cp budget.sh /usr/local/bin
+
+sudo chmod +x /usr/local/bin/budget.sh
+
+# Check to make sure the service name isn't used
+# sudo systemctl list-unit-files --type-service
+
+sudo cp budget.service /etc/systemd/system/budget.service
+sudo chmod 640 /etc/systemd/system/budget.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable budget
+sudo systemctl start budget
+# Can check status:
+# sudo systemctl status budget
+# Can stop:
+# sudo systemctl stop budget
+# Can disable:
+# sudo systemctl disable budget
+
